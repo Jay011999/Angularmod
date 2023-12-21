bac2c14240310136f98caa7eb6dbd1ceafae0e9d
Jay011999 <jay483338@gmail.com>
2023-12-21 23:09:19 +0530

    'updating_component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  products: any = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Description 1',
+      type: 'Mobile',
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'Description 2',
+      type: 'Desktop',
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      description: 'Description 3',
+      type: 'Laptop',
+    },
+  ];
 
+  isadmin=false;
+  somevalue='Ram_';
 }
