970b53fa2e5c3f37728e7af10114aca914eb408d
Jay011999 <jay483338@gmail.com>
2023-12-21 20:32:07 +0530

    'updating'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angularmod';
+
+  isvalid=true;
+  imgpath='../favicon.ico'
+
+  onClick(){
+    alert('button clicked!!')
+  }
 }
