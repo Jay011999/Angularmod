82c200fc4905dc344e0959049c3c479768992b1e
Jay011999 <jay483338@gmail.com>
2023-12-25 00:57:43 +0530

    updates

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,19 +1,20 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-  private url='https://localhost:3000';
+  private url = 'http://localhost:3000';
 
-  constructor(private http:HttpClient) { }
-  getProducts():Observable<any[]>{
-    return this.http.get<any[]>(this.url);
+  constructor(private http: HttpClient) {}
+  getProducts(category: string): Observable<any[]> {
+    const params = new HttpParams().set('category', category);
+    return this.http.get<any[]>(`${this.url}/products`, { params: params });
   }
 
-  getData(){
-    return "data";
+  getData() {
+    return 'data';
   }
 }
--- src/app/mobiles/mobiles.component.ts
+++ src/app/mobiles/mobiles.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-mobiles',
   templateUrl: './mobiles.component.html',
   styleUrl: './mobiles.component.css',
 })
-export class MobilesComponent implements OnInit{
-  constructor(private dataServices: DataService) {}
+export class MobilesComponent implements OnInit {
+  category:string='';
+  products!:any;
+  // subscription: Subscription | undefined;
+  constructor(private obj: DataService,private route:ActivatedRoute) {}
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params)=>{
+      this.category=params['category'];
+    })
+    this.obj.getProducts(this.category).subscribe({
+      next: (data)=>this.products=data,
+      error: (err) => console.log(err),
+      complete: () => console.log('complete'),
+    });
   }
 
   addItem() {
-    const data = this.dataServices.getData();
+    const data = this.obj.getData();
     console.log(data);
   }
 }
